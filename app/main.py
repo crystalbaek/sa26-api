5539f0229161eaed61d6813ef172f82a79ad7b50
crystalbaek <bfilkl730@gmail.com>
2023-10-25 12:53:08 +0900

    add dday

--- app/main.py
+++ app/main.py
@@ -14,3 +14,12 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+@app.get("/dday")
+def get_dday():
+    """
+    # D-DAY
+    ![LGTM](https://i.lgtm.fun/2lwa.gif)
+    """
+    from dday.dday import calculate_dday
+    r = calculate_dday()
+    return {"dday": r}
